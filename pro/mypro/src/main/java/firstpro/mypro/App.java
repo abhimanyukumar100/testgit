b24b79fdf6f43dce5275f766c84adb38f0688303
abhimanyukumar100 <kumar.abhimanyu68@gmail.com>
2018-11-25 17:19:39 +0530

    first commit

---
+++ pro/mypro/src/main/java/firstpro/mypro/App.java
@@ -0,0 +1,13 @@
+package firstpro.mypro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ pro/mypro/src/main/java/firstpro/mypro/Test.java
@@ -0,0 +1,10 @@
+package firstpro.mypro;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//hello
+	}
+
+}
