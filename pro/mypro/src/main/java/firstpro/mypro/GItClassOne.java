b3651e8de1a3e880e14eb0802cb89f3b978296f4
abhimanyukumar100 <kumar.abhimanyu68@gmail.com>
2018-12-09 18:44:52 +0530

    code commit 1

---
+++ pro/mypro/src/main/java/firstpro/mypro/GItClassOne.java
@@ -0,0 +1,11 @@
+package firstpro.mypro;
+
+public class GItClassOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.print  Hello git repository
+
+	}
+
+}
